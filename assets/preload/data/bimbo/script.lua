2681cdbd06e7555c91f0b905753b243eecc2912e
BeastlyGhost <lucariothelast91@gmail.com>
2022-04-02 17:33:59 -0400

    Fix Bimbo's notes

--- assets/preload/data/bimbo/script.lua
+++ assets/preload/data/bimbo/script.lua
@@ -1,29 +1,11 @@
 function onCreatePost()
-	if not getProperty('opponentMode') then
-		for i= 0, 3 do
-			setPropertyFromGroup('opponentStrums', i, 'texture', 'noteskins/opponent/bimbo')
-		end
+	for i= 0, 3 do
+		setPropertyFromGroup('opponentStrums', i, 'texture', 'noteskins/opponent/bimbo')
 	end
 
-	if getProperty('opponentMode') then
-		for i= 0, 3 do
-			setPropertyFromGroup('opponentStrums', i, 'texture', 'noteskins/opponent/boyfriend')
-		end
-	end
-	
-	if not getProperty('opponentMode') then
-		for i = 0, getProperty('unspawnNotes.length') - 1 do
-			if not getPropertyFromGroup('unspawnNotes', i, 'mustPress') then
-				setPropertyFromGroup('unspawnNotes', i, 'texture', 'noteskins/opponent/bimbo')
-			end
-		end
-	end
-
-	if getProperty('opponentMode') then
-		for i = 0, getProperty('unspawnNotes.length') - 1 do
-			if not getPropertyFromGroup('unspawnNotes', i, 'mustPress') then
-				setPropertyFromGroup('unspawnNotes', i, 'texture', 'noteskins/opponent/boyfriend')
-			end
+	for i = 0, getProperty('unspawnNotes.length') - 1 do
+		if not getPropertyFromGroup('unspawnNotes', i, 'mustPress') then
+			setPropertyFromGroup('unspawnNotes', i, 'texture', 'noteskins/opponent/bimbo')
 		end
 	end
 end
