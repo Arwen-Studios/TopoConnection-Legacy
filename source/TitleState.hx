53b6cf0dab43421d32aabea43d762ccc35036f1f
BeastlyGhost <lucariothelast91@gmail.com>
2022-04-27 16:12:04 -0400

    remove title easter eggs

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -73,7 +73,7 @@ class TitleState extends MusicBeatState
 
 	#if TITLE_SCREEN_EASTER_EGG
 	var easterEggKeys:Array<String> = [
-		'SHADOW', 'RIVER', 'SHUBS', 'BBPANZU'/*, 'OSUOK'*/
+		'SHADOW', 'RIVER', 'SHUBS', 'BBPANZU'
 	];
 	var allowedKeys:String = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	var easterEggKeysBuffer:String = '';
@@ -312,11 +312,6 @@ class TitleState extends MusicBeatState
 				gfDance.frames = Paths.getSparrowAtlas('BBBump');
 				gfDance.animation.addByIndices('danceLeft', 'BB Title Bump', [14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27], "", 24, false);
 				gfDance.animation.addByIndices('danceRight', 'BB Title Bump', [27, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], "", 24, false);
-			/*case 'OSUOK':
-				gfDance.frames = Paths.getSparrowAtlas('SuokBump');
-				gfDance.animation.addByPrefix('danceLeft', 'Suok Title Bump', 24, false);
-				gfDance.animation.addByPrefix('danceRight', 'Suok Title Bump', 24, false);
-			*/
 			#end
 
 			default:
