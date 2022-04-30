f7d124c990ffdade1ea21a2254a3c5a7e6c30692
BeastlyGhost <45212377+BeastlyGhost@users.noreply.github.com>
2022-04-30 12:35:35 -0400

    uh

--- source/data/GJKeysButGaming.hx
+++
@@ -1,9 +0,0 @@
-// empty GJ keys so Github Workflows will shut up about it - BeastlyGhost
-
-package data;
-
-class GJKeysButGaming
-{
-    public static var id:Int = 	-1; // dumb.
-    public static var key:String = "";
-}
